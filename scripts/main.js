0915a2b16159788205c6dc3a0fc38c76ed9ad9c5
Arbitermartin <arbitermartin04@gmail.com>
2025-02-26 22:26:42 +0300

    add changes

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,11 @@ document.getElementById('toggleButton').addEventListener('click',function(){
     }
     navMenu.classList.toggle('show');
 });
+// year
+const year = document.querySelector("#currentYear");
+const today = new Date();
+year.innerHTML = `<span>${today.getFullYear()}</span>`;
+
+//last modified
+const date = document.querySelector("#lastModified");
+date.innerHTML = `<span>${document.lastModified}<span>`;
